413e1a7e18e3f05625f568539249c1de775f63b5
chtiwa <chtiwaa@gmail.com>
2025-06-18 17:54:49 +0100

    fixed bug v3

--- middleware/corsMiddlware.go
+++ middleware/corsMiddlware.go
@@ -12,8 +12,9 @@ func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		origin := c.Request.Header.Get("Origin")
 		clientURL := os.Getenv("CLIENT_URL")
+		adminURL := os.Getenv("ADMIN_URL")
 
-		if origin == clientURL || origin == "http://localhost:5000" {
+		if origin == clientURL || origin == adminURL || origin == "http://localhost:5000" {
 			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
 			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 			c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH, OPTIONS")
